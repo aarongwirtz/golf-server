0cb9da13093375b9c7cd229c1434fb833914e6f3
Keagan Talbot <keaganpt@gmail.com>
2020-11-23 16:40:00 -0500

    db made and connected

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
+const db = require('./db');
+
+app.use(express.json())
+db.sync();
+
 
 app.listen(3000, function() {
     console.log('App listening on port 3000.');
