fce50cb6c1778226d4db882c268b87fa0180cc7d
Keagan Talbot <keaganpt@gmail.com>
2020-11-23 21:26:33 -0500

    scoreController delete, put, and get by user

--- controllers/scorecardController.js
+++ controllers/scorecardController.js
@@ -13,7 +13,9 @@ router.get('/', (req,res) => {
 
 //get all of 1 user's scorecards
 router.get('/:username', (req,res) => {
-
+    Scorecard.findAll({ where: {userName: req.params.userName}})
+        .then(scorecard => res.status(200).json(scorecard))
+        .catch(err => res.status(500).json({ error: err}))
 })
 
 //create a scorecard
@@ -23,12 +25,27 @@ router.post('/create', (req,res) => {
 
 //update 1 scorecard at id
 router.put('/:id', (req,res) => {
-
-})
+    const query = req.params.id;
+    Scorecard.update(req.body, {where: {id: query}})
+        .then((scorecardUpdated) => {
+            Scorecard.findOne({ where : {id: query} }).then((locatedUpdateScorecard) => {
+                res.status(200).json({
+                    scorecard: locatedUpdateScorecard,
+                    message: "Scorecard updated successfully",
+                    scorecardChanged: scorecardUpdated
+                })
+            })
+        })
+        .catch((err) => res.json({ error: err }))
+});
 
 //delete scorecard at target id
 router.delete('/:id', (req,res) => {
-    
+    Scorecard.destroy({
+        where: {id: req.params.id}
+    })
+    .then(scorecard => res.status(200).json(scorecard))
+    .catch(err => res.json({ error: err}))
 })
 
 module.exports = router
