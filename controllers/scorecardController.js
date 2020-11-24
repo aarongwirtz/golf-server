0efc3b73765c4bf0e20e2a1553cd758301f743ad
Keagan Talbot <keaganpt@gmail.com>
2020-11-24 10:56:07 -0500

    validate session complete, scorecontroller protected

--- controllers/scorecardController.js
+++ controllers/scorecardController.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {Scorecard} = require('../models');
 
+const validateSession = require('../middleware/validateSession');
+
 router.get('/golflove', (req, res) => {
     res.send("testing what I remember")
 })
 
 //get all scorecards
-router.get('/', (req,res) => {
+router.get('/', validateSession, (req,res) => {
     Scorecard.findAll()
     .then(scorecard => res.status(200).json(scorecard))
     .catch(err => res.status(500).json({
@@ -15,18 +17,18 @@ router.get('/', (req,res) => {
     }))
 })
 //get all of 1 user's scorecards
-router.get('/:userName', (req,res) => {
+router.get('/:userName', validateSession, (req,res) => {
     Scorecard.findAll({ where: {userName: req.params.userName}})
         .then(scorecard => res.status(200).json(scorecard))
         .catch(err => res.status(500).json({ error: err}))
 })
 
 //create a scorecard
-router.post('/create', async (req,res) => {
+router.post('/create',validateSession, async (req,res) => {
     try{
         const {userName, courseName, date, conditions, courseLength, difficultyRating, h1Score, h2Score, h3Score, h4Score, h5Score, h6Score, h7Score, h8Score, h9Score, h10Score, h11Score, h12Score, h13Score, h14Score, h15Score, h16Score, h17Score, h18Score, totalScore, h1Par, h2Par, h3Par, h4Par, h5Par, h6Par, h7Par, h8Par, h9Par, h10Par, h11Par, h12Par, h13Par, h14Par, h15Par, h16Par, h17Par, h18Par, totalPar} = req.body;
 
-        let newScore = await Scorecard.create({userName: req.user.username, courseName, date, conditions, courseLength, difficultyRating, h1Score, h2Score, h3Score, h4Score, h5Score, h6Score, h7Score, h8Score, h9Score, h10Score, h11Score, h12Score, h13Score, h14Score, h15Score, h16Score, h17Score, h18Score, totalScore, h1Par, h2Par, h3Par, h4Par, h5Par, h6Par, h7Par, h8Par, h9Par, h10Par, h11Par, h12Par, h13Par, h14Par, h15Par, h16Par, h17Par, h18Par, totalPar
+        let newScore = await Scorecard.create({userName : req.user.username, courseName, date, conditions, courseLength, difficultyRating, h1Score, h2Score, h3Score, h4Score, h5Score, h6Score, h7Score, h8Score, h9Score, h10Score, h11Score, h12Score, h13Score, h14Score, h15Score, h16Score, h17Score, h18Score, totalScore, h1Par, h2Par, h3Par, h4Par, h5Par, h6Par, h7Par, h8Par, h9Par, h10Par, h11Par, h12Par, h13Par, h14Par, h15Par, h16Par, h17Par, h18Par, totalPar
         })
         res.status(200).json({
             score: newScore,
@@ -41,7 +43,7 @@ router.post('/create', async (req,res) => {
 });
 
 //update 1 scorecard at id
-router.put('/:id', (req,res) => {
+router.put('/:id', validateSession, (req,res) => {
     const query = req.params.id;
     Scorecard.update(req.body, {where: {id: query}})
         .then((scorecardUpdated) => {
@@ -57,7 +59,7 @@ router.put('/:id', (req,res) => {
 });
 
 //delete scorecard at target id
-router.delete('/:id', (req,res) => {
+router.delete('/:id', validateSession, (req,res) => {
     Scorecard.destroy({
         where: {id: req.params.id}
     })
