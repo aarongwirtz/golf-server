fc2a55a8249d884f650788a67f8a6a9eb5309170
caseyeschmann <ceschmann@msn.com>
2020-11-24 10:19:39 -0500

    updated usercontroller w/ bcrypt, token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const {User} = require('../models');
+const jwt = require ("jsonwebtoken");
+const bcrypt = require('bcryptjs');
 
 // Create User
 
@@ -8,12 +10,17 @@ router.post('/register', function(req,res) {
     User.create({
         email: req.body.user.email,
         username: req.body.user.username,
-        password: req.body.user.password
+        password: bcrypt.hashSync(req.body.user.password, 13)
     })
     .then(
         function userRegistered(user) {
+
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+
             res.json({
-                user: user
+                user: user,
+                message: "User created!",
+                sessionToken: token
             });
         }
     )
@@ -31,12 +38,28 @@ router.post('/login', function(req, res) {
             username: req.body.user.username
         }
     })
+
     .then(function userLoggedIn(user) {
+        
         if (user) {
+        
+        bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
+
+        if (matches) {
+
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+
             res.status(200).json({
                 user: user,
-                message: 'User successfully logged in!'
+                message: "User successfully logged in!",
+                sessionToken: token
             })
+
+            }    else {
+            res.status(502).send({ error: "Login failed" })
+        }
+        });
+
         } else {
             res.status(500).json({ error: 'Cannot find user info.' })
         }
