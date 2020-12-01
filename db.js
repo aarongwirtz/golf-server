14b25c652578c747b9ada207a44f9cec69dd71a5
Keagan Talbot <keaganpt@gmail.com>
2020-12-01 17:11:47 -0500

    working personalCardEndpoint

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-<<<<<<< HEAD
     dialect: 'postgres'
-=======
-    dialect: 'postgres',
->>>>>>> 781a5e5ffb87b1efbd91ef3f35d7107553a3033f
 });
 
 module.exports = db;
